ee8dae858495c690518c103653163a1a9d6a651d
Rory Molyneux <rory@pitchdark.co.za>
2018-05-20 13:06:40 +0200

    Field values and WYSIWYG editor added to the form

--- pd-admin-options.php
+++ pd-admin-options.php
@@ -39,18 +39,23 @@ class PD_Category_Auto_Emailer_Admin {
         $html .= wp_nonce_field('pd-category-auto-emailer-save_options', '_wpnonce', true, false);
         $html .= '<table class="form-table">';
         $html .= '<tr>';
+        $html .= '<th><label for="pd-auto-from-name">From Name</label></th>';
+        $html .= '<td><input name="pd-auto-from-name" type="email" id="pd-auto-from-name" class="regular-text" value="' . $this->get_pd_from_name() . '" /><p class="description">The displayed From Name on the notifications</p></td>';
+        $html .= '</tr>';
+        $html .= '<tr>';
         $html .= '<th><label for="pd-auto-from">From Address</label></th>';
-        $html .= '<td><input name="pd-auto-from" type="email" id="pd-auto-from" class="regular-text" value="" /><p class="description">The displayed From and Reply-To email address on the notification</p></td>';
+        $html .= '<td><input name="pd-auto-from" type="email" id="pd-auto-from" class="regular-text" value="' . $this->get_pd_email() . '" /><p class="description">The displayed From and Reply-To email address on the notification</p></td>';
         $html .= '</tr>';
         $html .= '<tr>';
         $html .= '<th><label for="pd-auto-bcc">Bcc Address(es):</label></th>';
-        $html .= '<td><input name="pd-auto-bcc" type="text" id="pd-auto-bcc" class="regular-text" value="" /><p class="description">Comma-separated list of any additional addresses you would like to add to any notification</p></td>';
+        $html .= '<td><input name="pd-auto-bcc" type="text" id="pd-auto-bcc" class="regular-text" value="' . $this->get_pd_bcc() . '" /><p class="description">Comma-separated list of any additional addresses you would like to add to any notification</p></td>';
         $html .= '</tr>';
         $html .= '<tr>';
         $html .= '<th><label for="pd-auto-body">HTML Emailer Body</label></th>';
         $html .= '<td>';
+        $html .= $this->body_content_field();
+        //$html .= '<textarea name="pd-auto-body" id="pd-auto-body" class="regular-text" style="width: 100%" rows="20"></textarea>';
         $html .= '<p class="description">The following aliases will be substituted with content:<br/>$$BLOGNAME$$, $$PERMALINK$$, $$POSTNAME$$, $$POSTTITLE$$<br/>Use them to substitute dynamic content in your email</p>';
-        $html .= '<textarea name="pd-auto-body" id="pd-auto-body" class="regular-text" style="width: 100%" rows="20"></textarea>';
         $html .= '</td>';
         $html .= '</tr>';
         $html .= '</table>';
@@ -60,6 +65,63 @@ class PD_Category_Auto_Emailer_Admin {
         echo $html;
     }
     
+    private function get_pd_from_name() {
+        
+        $from_name = get_option('pd_from_name', get_bloginfo('name'));
+        
+        return $from_name;        
+        
+    }
+    
+    private function get_pd_email() {
+        
+        $from_email = get_option('pd_from_email', get_option('admin_email'));
+        
+        return $from_email;
+        
+    }
+    
+    private function get_pd_bcc() {
+        
+        $bcc_emails = get_option('pd_bcc', '');
+        
+        return $bcc_emails;
+        
+    }
+    
+    private function get_pd_body_content() {
+        
+        $body_content = get_option('pd_body_content', $this->default_body_content());
+        
+        return $body_content;
+        
+    }
+    
+    private function default_body_content() {
+        $html = 'Hi there,
+A new article relevant to you has been posted on $$BLOGNAME$$. Click the link to read:
+<b><a href="$$PERMALINK$$" title="$$POSTNAME$$">$$POSTTITLE$$</a></b>
+<b>We welcome daily  news for placement</b> and we invite you to send us any events as they happen.
+
+Kind regards,
+The <b>$$BLOGNAME$$</b> Team';
+        
+        return $html;
+        
+    }
+    
+    private function body_content_field() {
+        ob_start();
+        wp_editor($this->get_pd_body_content(), 'pd-auto-body', array(
+            'wpautop' => false,
+            'textarea_name' => 'pd-auto-body',
+            'media_buttons' => false
+        ));
+        $ret = ob_get_contents();  
+        ob_end_clean();  
+        return $ret;   
+        
+    }
     
     
 }
