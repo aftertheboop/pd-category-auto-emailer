ee54e089a4eebb5139da2b71857f661ee1e79211
Rory Molyneux <rory@pitchdark.co.za>
2018-05-20 14:10:29 +0200

    Options save to wp_options table

--- pd-admin-options.php
+++ pd-admin-options.php
@@ -1,11 +1,5 @@
 <?php
 
-/* 
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-
 $pd_category_auto_emailer_admin = new PD_Category_Auto_Emailer_Admin();
 
 class PD_Category_Auto_Emailer_Admin {
@@ -24,8 +18,103 @@ class PD_Category_Auto_Emailer_Admin {
     
     public function pd_auto_emailer_admin_init() {
         
+        if ( !current_user_can( 'manage_options' ) )  {
+            wp_die( __( 'You do not have sufficient permissions to access this page.' ) );
+        }
+        
+        // Save options
+        if(isset($_POST['option_page'])) {
+            $this->admin_save_options();    
+        }
+        
+        // Display form
         $this->admin_menu_html();
         
+    }
+    
+    private function admin_save_options() {
+        
+        $this->set_pd_from_name();
+        $this->set_pd_from_email();
+        $this->set_pd_bcc();
+        $this->set_pd_body_content();
+        
+        
+    }
+    
+    private function set_pd_from_name() {
+        
+        $option_name = 'pd_from_name' ;
+        $new_value = filter_input(INPUT_POST, 'pd-auto-from-name', FILTER_SANITIZE_STRING) ;
+
+        if ( get_option( $option_name ) !== false ) {
+
+            // The option already exists, so we just update it.
+            update_option( $option_name, $new_value );
+
+        } else {
+
+            // The option hasn't been added yet. We'll add it with $autoload set to 'no'.
+            $deprecated = null;
+            $autoload = 'no';
+            add_option( $option_name, $new_value, $deprecated, $autoload );
+        }
+    }
+    
+    private function set_pd_from_email() {
+        
+        $option_name = 'pd_from_email' ;
+        $new_value = filter_input(INPUT_POST, 'pd-auto-from', FILTER_SANITIZE_STRING) ;
+
+        if ( get_option( $option_name ) !== false ) {
+
+            // The option already exists, so we just update it.
+            update_option( $option_name, $new_value );
+
+        } else {
+
+            // The option hasn't been added yet. We'll add it with $autoload set to 'no'.
+            $deprecated = null;
+            $autoload = 'no';
+            add_option( $option_name, $new_value, $deprecated, $autoload );
+        }
+    }
+    
+    private function set_pd_bcc() {
+        
+        $option_name = 'pd_bcc' ;
+        $new_value = filter_input(INPUT_POST, 'pd-auto-bcc', FILTER_SANITIZE_STRING) ;
+
+        if ( get_option( $option_name ) !== false ) {
+
+            // The option already exists, so we just update it.
+            update_option( $option_name, $new_value );
+
+        } else {
+
+            // The option hasn't been added yet. We'll add it with $autoload set to 'no'.
+            $deprecated = null;
+            $autoload = 'no';
+            add_option( $option_name, $new_value, $deprecated, $autoload );
+        }
+    }
+    
+    private function set_pd_body_content() {
+        $option_name = 'pd_body_content' ;
+        $new_value = filter_input(INPUT_POST, 'pd-auto-body') ;
+
+        if ( get_option( $option_name ) !== false ) {
+
+            // The option already exists, so we just update it.
+            update_option( $option_name, $new_value );
+
+        } else {
+
+            // The option hasn't been added yet. We'll add it with $autoload set to 'no'.
+            $deprecated = null;
+            $autoload = 'no';
+            add_option( $option_name, $new_value, $deprecated, $autoload );
+        }
     }
     
     private function admin_menu_html() {
@@ -33,14 +122,19 @@ class PD_Category_Auto_Emailer_Admin {
         $html = '<div class="wrap">';
         $html .= '<h1>Category Auto Emailer Settings</h1>';
         $html .= '<p class="description">Configure settings for your category auto-emailer. These settings are universal across all categories.</p>';
-        $html .= '<form method="post" action="options.php" novalidate="novalidate">';
-        $html .= '<input type="hidden" name="option_page" value="general">';
+        $html .= '<form id="pd-auto-emailer" method="post">';
+        
+        echo $html;
+        
+        settings_fields( 'pd-auto-emailer-settings' );
+        do_settings_sections( 'pd-auto-emailer-settings' ); 
+        /*$html .= '<input type="hidden" name="option_page" value="general">';
         $html .= '<input type="hidden" name="action" value="update">';
-        $html .= wp_nonce_field('pd-category-auto-emailer-save_options', '_wpnonce', true, false);
-        $html .= '<table class="form-table">';
+        $html .= wp_nonce_field('pd-category-auto-emailer-save_options', '_wpnonce', true, false);*/
+        $html = '<table class="form-table">';
         $html .= '<tr>';
         $html .= '<th><label for="pd-auto-from-name">From Name</label></th>';
-        $html .= '<td><input name="pd-auto-from-name" type="email" id="pd-auto-from-name" class="regular-text" value="' . $this->get_pd_from_name() . '" /><p class="description">The displayed From Name on the notifications</p></td>';
+        $html .= '<td><input name="pd-auto-from-name" type="text" id="pd-auto-from-name" class="regular-text" value="' . $this->get_pd_from_name() . '" /><p class="description">The displayed From Name on the notifications</p></td>';
         $html .= '</tr>';
         $html .= '<tr>';
         $html .= '<th><label for="pd-auto-from">From Address</label></th>';
@@ -59,9 +153,10 @@ class PD_Category_Auto_Emailer_Admin {
         $html .= '</td>';
         $html .= '</tr>';
         $html .= '<tr>';
-        $html .= '<td></td><td><input type="submit" name="submit" value="Save Options" class="button button-primary button-large" /></td>';
+        $html .= '<td></td><td>' . get_submit_button('Save Options', 'primary large', 'pd_save_options') . '</td>';
         $html .= '</tr>';
         $html .= '</table>';
+        $html .= '</form>';
         $html .= '</div>';
         
         
